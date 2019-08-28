928690049351d8dcef7f77662fd6415c1c05d8e4
Hayton Jake <Jake.Hayton@nhsbt.nhs.uk>
2019-08-28 10:14:33 +0100

    Add project files.

---
+++ BrightHRKataLibrary/CheckoutLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BrightHRKataLibrary
+{
+    public class CheckoutLibrary
+    {
+        interface ICheckout
+        {
+            void Scan(string item);
+            int GetTotalPrice();
+        }
+
+        void Scan (string item)
+        {
+
+        }
+
+        int GetToalPrice()
+        {
+
+            return 0;
+        }
+    }
+}
